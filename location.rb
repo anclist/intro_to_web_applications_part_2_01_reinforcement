464c2d44025e0a44af3766b0a16cf227872e3e06
Tony <ANCLIST@GMAIL.COM>
2017-10-04 19:17:23 -0400

    Add detination instances to destination array

--- location.rb
+++ location.rb
@@ -1,4 +1,30 @@
 class Location
+  attr_accessor(:name)
+
   def initialize(name)
     @name = name
+  end
+
 end
+
+class Trips
+
+  attr_accessor(:destinations)
+
+  def initialize
+    @destinations = []
+  end
+
+  def destination(name)
+    new_destination = Location.new(name)
+    @destinations << new_destination
+    new_destination
+  end
+end
+
+Trips.new.destination("Toronto")
+Trips.new.destination("Ottawa")
+Trips.new.destination("Montreal")
+Trips.new.destination("Quebec City")
+Trips.new.destination("Halifax")
+Trips.new.destination("St. John's")
